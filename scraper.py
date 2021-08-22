7cf4fcee8907462e0d24e9dfdd18a6a9d1f0cb61
Josh <j.j.h.lindner@gmail.com>
2021-08-22 15:33:31 -0700

    Create scraper.py.

---
+++ scraper.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options 
+
+
+chrome_options = Options()  
+chrome_options.add_argument("--headless") 
+browser = webdriver.Chrome(chrome_options=chrome_options)
+
+url = "https://www.youtube.com/user/spacexchannel/live"
+
+
+browser.get(url)
