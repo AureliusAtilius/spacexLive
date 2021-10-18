d8e9ca202493d5b85b79f07035bfa993743b7d9b
Josh <j.j.h.lindner@gmail.com>
2021-10-17 21:52:39 -0700

    Build out YouTuber class.

--- liveYT.py
+++ liveYT.py
@@ -4,13 +4,41 @@ def isLive(notLive, reqLen):
         
         return int(notLive)*2 < int(reqLen)
 
-                
-def isUserLive(GOOGLE_API,USER_ID):
-        reader = codecs.getreader('utf-8')
-        data = json.load(reader(urllib.request.urlopen('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={}&type=video&eventType=live&key={}'.format(USER_ID, GOOGLE_API))))
-        if len(data['items']) == 0:
-                return False
-        else:
-                return True
+
+class YouTuber:
+
+    global GOOGLE_API
+    global YouTubeName
+    global PlaylistID
+    global data
+    global videosData
+    global oldVideosData
+    global userID
+    global liveId
+    global reader
+    isID = False
+
+    def __init__(self, GOOGLE_API_KEY, User, isID = False):
+
+        self.reader = codecs.getreader('utf-8')
+
+        self.GOOGLE_API = GOOGLE_API_KEY
+        self.YouTubeName = User
+        self.isID = isID
+
+        if not self.isID:
+            self.userID = self.getUserID()
+
+        self.PlaylistID = self.setPlaylistID()
+        self.data = self.getPlaylistData()
+        self.videosData = self.getVideosData(self.data)
+                        
+        def isUserLive(self):
+                data = json.load(self.reader(urllib.request.urlopen('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={}&type=video&eventType=live&key={}'.format(self.getUserID(), self.GOOGLE_API))))
+                if len(data['items']) == 0:
+                        return False
+                if not len(data['items']) == 0:
+                        self.liveId = self.getUserLiveData()
+                        return True
 
         
